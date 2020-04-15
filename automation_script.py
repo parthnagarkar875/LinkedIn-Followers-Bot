a98dfaeaf1656e2953202d4a87c94e5e493a94dd
VirtualGoat <nagarkarparth875@gmail.com>
2020-04-15 23:45:52 +0530

    Finished automation

--- automation_script.py
+++ automation_script.py
@@ -11,94 +11,53 @@ from time import sleep, strftime
 from random import randint
 import pandas as pd
 
-chromedriver_path = '/usr/bin/chromedriver' # Change this to your own chromedriver path!
+chromedriver_path = 'C:/Users/Parth/Contacts/Downloads/chromedriver_win32/chromedriver.exe'
+#chromedriver_path = 'C:\Users\Parth\Contacts\Downloads\chromedriver_win32\chromedriver.exe' # Change this to your own chromedriver path!
 webdriver = webdriver.Chrome(executable_path=chromedriver_path)
 sleep(2)
-webdriver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
+webdriver.get('https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin')
 sleep(3)
 
-username = webdriver.find_element_by_name('username')
+# =============================================================================
+# Login
+# =============================================================================
+username = webdriver.find_element_by_xpath('/html/body/div/main/div[2]/form/div[1]/input')
 username.send_keys('')  #specify username
-password = webdriver.find_element_by_name('password')
+password = webdriver.find_element_by_xpath('/html/body/div/main/div[2]/form/div[2]/input')
 password.send_keys('')  #specify password
-button_login = webdriver.find_element_by_css_selector('#react-root > section > main > div > article > div > div > div > form > div > button')
+sleep(5)
+try:
+    button_login = webdriver.find_element_by_xpath('/html/body/div/main/div[2]/form/div[3]/button')
+except:
+    button_login = webdriver.find_element_by_xpath('/html/body/div/main/div[2]/form/div[4]/button')
 button_login.click()
 sleep(3)
-notnow = webdriver.find_element_by_css_selector('body > div.RnEpo.Yx5HN> div > div > div.mt3GC > button.aOOlW.HoLwm')
-notnow.click() #comment these last 2 lines out, if you don't get a pop up asking about notifications
+
+# =============================================================================
+# Navigate to your connections
+# =============================================================================
+notnow = webdriver.find_element_by_xpath('/html/body/header/div/nav/ul/li[2]/a/span[1]')
+notnow.click() 
+sleep(4)
+seeall = webdriver.find_element_by_xpath('/html/body/div[5]/div[3]/div[3]/div/div/div/div/div/div/ul/li[1]/div/button/span')
+seeall.click() 
+sleep(4)
 
 
-hashtag_list = ['fcbarcelona', 'chelsea', 'manchestercity']
+# =============================================================================
+# Rapidly send connection requests
+# =============================================================================
+for i in range(1,1000):
+    sleep(2)
+    one="/html/body/div[4]/div/div/div[2]/ul/li["
+    two="]/div/section/div[2]/footer/button/span"
+    total=one+str(i)+two
+    connect = webdriver.find_element_by_xpath(total)
+    connect.click() 
+    if i%12==0:
+        sleep(3)
+    
 
-prev_user_list = [] #- if it's the first time you run it, use this line and comment the two below
-# prev_user_list = pd.read_csv('20181203-224633_users_followed_list.csv', delimiter=',').iloc[:,
-#                  1:2]  # useful to build a user log
-# prev_user_list = list(prev_user_list['0'])
 
-new_followed = []
-tag = -1
-followed = 0
-likes = 0
-comments = 0
 
-for hashtag in hashtag_list:
-    tag += 1
-    webdriver.get('https://www.instagram.com/explore/tags/' + hashtag_list[tag] + '/')
-    sleep(5)
-    first_thumbnail = webdriver.find_element_by_xpath(
-        '//*[@id="react-root"]/section/main/article/div[1]/div/div/div[1]/div[1]/a/div')
-    first_thumbnail.click()
-    sleep(randint(1, 2))
-    try:
-        for x in range(1, 200):
-            username = webdriver.find_element_by_xpath(
-                '/html/body/div[3]/div[2]/div/article/header/div[2]/div[1]/div[1]/h2/a').text
 
-            if username not in prev_user_list:
-                # If we already follow, do not unfollow
-                if webdriver.find_element_by_xpath(
-                        '/html/body/div[3]/div[2]/div/article/header/div[2]/div[1]/div[2]/button').text == 'Follow':
-
-                    webdriver.find_element_by_xpath(
-                        '/html/body/div[3]/div[2]/div/article/header/div[2]/div[1]/div[2]/button').click()
-
-                    new_followed.append(username)
-                    followed += 1
-
-                    # Liking the picture
-                    button_like = webdriver.find_element_by_xpath(
-                        '/html/body/div[3]/div[2]/div/article/div[2]/section[1]/span[1]/button/span')
-
-                    button_like.click()
-                    likes += 1
-                    sleep(randint(18, 25))
-                    comments += 1
-                    webdriver.find_element_by_xpath(
-                            '/html/body/div[3]/div[2]/div/article/div[2]/section[1]/span[2]/button/span').click()
-                    comment_box = webdriver.find_element_by_xpath(
-                            '/html/body/div[3]/div[2]/div/article/div[2]/section[3]/div/form/textarea')
-                    comment_box.send_keys('Really cool!')
-                    sleep(3)
-
-                # Enter to post comment
-                    comment_box.send_keys(Keys.ENTER)
-                    sleep(randint(22, 28))
-                # Next picture
-                webdriver.find_element_by_link_text('Next').click()
-                sleep(randint(25, 29))
-            else:
-                webdriver.find_element_by_link_text('Next').click()
-                sleep(randint(20, 26))
-                # sleep(2)
-    # some hashtag stops refreshing photos (it may happen sometimes), it continues to the next
-    except:
-        continue
-
-for n in range(0, len(new_followed)):
-    prev_user_list.append(new_followed[n])
-
-updated_user_df = pd.DataFrame(prev_user_list)
-updated_user_df.to_csv('{}_users_followed_list.csv'.format(strftime("%Y%m%d-%H%M%S")))
-print('Liked {} photos.'.format(likes))
-print('Commented {} photos.'.format(comments))
-print('Followed {} new people.'.format(followed))
