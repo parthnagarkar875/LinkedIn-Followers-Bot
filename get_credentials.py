dc6a6207abfec8a28b50931fb2d85a0ac4de6af5
Yatin T <yatint5@gmail.com>
2020-06-07 10:44:41 +0530

    bug fix

--- get_credentials.py
+++ get_credentials.py
@@ -1,24 +1,25 @@
 import pickle
 
-print('Please enter the following information:')
-credentials = {}
+def get_creds():
+	print('Please enter the following information:')
+	credentials = {}
 
 
-print('Which browser is installed on your system? Chrome or Firefox (0 for Chrome, 1 for Firefox)')
-ans = input()
-if ans == '0':
-	credentials['Browser'] = 'Chrome'
-else:
-	credentials['Browser'] = 'Firefox'
+	print('Which browser is installed on your system? Chrome or Firefox (0 for Chrome, 1 for Firefox)')
+	ans = input()
+	if ans == '0':
+		credentials['Browser'] = 'Chrome'
+	else:
+		credentials['Browser'] = 'Firefox'
 
-print('Please enter the path to geckodriver')
-credentials['EXECUTABLE_PATH'] = input()
+	print('Please enter the path to geckodriver')
+	credentials['EXECUTABLE_PATH'] = input()
 
-print("Please enter your LinkedIn Email/Phone No")
-credentials['Email_or_phone_no'] = input()
+	print("Please enter your LinkedIn Email/Phone No")
+	credentials['Email_or_phone_no'] = input()
 
-print("Please enter your password")
-credentials['Password'] = input()
+	print("Please enter your password")
+	credentials['Password'] = input()
 
-with open('credentials.pickle','wb') as f:	
-	pickle.dump(credentials,f)
+	with open('credentials.pickle','wb') as f:	
+		pickle.dump(credentials,f)
